a026dc93fff93f136b142db8f108120db7b60664
mvargasv95 <minorvargas46@gmail.com>
2020-01-26 21:38:27 -0600

    eslint configured :dog2:

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 const App = () => {
-  return (
-    <h1>This is a dogegram!</h1>
-  )
+  return <h1>This is a dogegram!</h1>
 }
 
-export default App;
+export default App
