4f17c975a943a40d0b44f1946d80d1b954aea7e8
mvargasv95 <minorvargas46@gmail.com>
2020-03-15 19:57:00 -0600

    fonts working

--- src/fonts/fonts.js
+++ src/fonts/fonts.js
@@ -1,7 +1,11 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 import PressStart from './pressStart.ttf'
 
 export default createGlobalStyle`
+    html, body {
+      margin: 0;
+      padding: 0;
+    }
     @font-face {
         font-family: 'Press Start';
         src: local('Press Start'), local('PressStart'),
